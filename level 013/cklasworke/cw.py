fdbdcc06d298ed6d34ef81900b5cfa11adc4b05a
OLIKOGOGLA <olikogogla@gmail.com>
2026-06-17 06:47:25 -0700

    Add files via upload

---
+++ level 013/cklasworke/cw.py
@@ -0,0 +1,13 @@
+<<<<<<< HEAD
+for i in range(7):
+    print("goa")
+
+for i in range(9):
+    print(i)
+=======
+for i in range(7):
+    print("goa")
+
+for i in range(9):
+    print(i)
+>>>>>>> 8a15946c560378624351c12a8843fbaa5f3d43ac
