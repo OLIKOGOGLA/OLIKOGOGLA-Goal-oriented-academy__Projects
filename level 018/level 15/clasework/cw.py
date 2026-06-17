74e244a90637eafca6e042ee160a4f525e262757
OLIKOGOGLA <olikogogla@gmail.com>
2026-06-17 06:43:52 -0700

    Add files via upload

---
+++ level 018/level 15/clasework/cw.py
@@ -0,0 +1,28 @@
+<<<<<<< HEAD
+# 1. ტემპერატურის შემოწმება
+temperature = int(input("შეიყვანეთ ტემპერატურა: "))
+if temperature > 30:
+    print("it's hot outside")
+else:
+    print("it's not too hot outside")
+
+# 2. ასაკის შემოწმება
+age = int(input("შეიყვანეთ თქვენი ასაკი: "))
+if age > 19:
+    print("adult")
+else:
+=======
+# 1. ტემპერატურის შემოწმება
+temperature = int(input("შეიყვანეთ ტემპერატურა: "))
+if temperature > 30:
+    print("it's hot outside")
+else:
+    print("it's not too hot outside")
+
+# 2. ასაკის შემოწმება
+age = int(input("შეიყვანეთ თქვენი ასაკი: "))
+if age > 19:
+    print("adult")
+else:
+>>>>>>> 8a15946c560378624351c12a8843fbaa5f3d43ac
+    print("teenager")
---
+++ level 025/kclaswork/cw.py
@@ -0,0 +1,115 @@
+<<<<<<< HEAD
+#შექმენით 3 ცვლადი, პირველში და მეორეში მომხმარებელმა უნდა შემოიტანოს რიცხვი ხოლო მესამეში რაიმე ოპერატორი 
+# (-, +, /, *) გააკეთეთ კალკულატორის სადაც პირველ რიცხვს გამრავლდება, გაიყოფა, მიემატება ან გამოაკლდება მეორე რიცხვი
+
+age=int(input("Enter one number"))
+age_=int(input("Enter one number"))
+Signs_of_action=input("Signs of action (-,+,/,*)")
+
+if Signs_of_action == "-":
+    print(age - age_ )
+elif Signs_of_action == "+":
+    print(age + age_ )
+elif Signs_of_action == "*":
+    print(age * age_ )
+elif Signs_of_action == "/":
+    print(age / age_ )
+elif age_ =="0":
+    print("error")
+#3) დაწერეთ while ციკლი სადაც მომხმარებელს შემოატანინებს სიტყვას მანამდე სანამ მისი სიტყვა არ დაემთხვევა password - ს 
+
+mane=input("Enter your password ")
+password="New Year"
+
+if mane == password:
+    print("You are right")
+elif mane != password:
+    print("you are Wrong")
+    mane=input("Enter your password ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=======
+#შექმენით 3 ცვლადი, პირველში და მეორეში მომხმარებელმა უნდა შემოიტანოს რიცხვი ხოლო მესამეში რაიმე ოპერატორი 
+# (-, +, /, *) გააკეთეთ კალკულატორის სადაც პირველ რიცხვს გამრავლდება, გაიყოფა, მიემატება ან გამოაკლდება მეორე რიცხვი
+
+age=int(input("Enter one number"))
+age_=int(input("Enter one number"))
+Signs_of_action=input("Signs of action (-,+,/,*)")
+
+if Signs_of_action == "-":
+    print(age - age_ )
+elif Signs_of_action == "+":
+    print(age + age_ )
+elif Signs_of_action == "*":
+    print(age * age_ )
+elif Signs_of_action == "/":
+    print(age / age_ )
+elif age_ =="0":
+    print("error")
+#3) დაწერეთ while ციკლი სადაც მომხმარებელს შემოატანინებს სიტყვას მანამდე სანამ მისი სიტყვა არ დაემთხვევა password - ს 
+
+mane=input("Enter your password ")
+password="New Year"
+
+if mane == password:
+    print("You are right")
+elif mane != password:
+    print("you are Wrong")
+    mane=input("Enter your password ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+>>>>>>> 8a15946c560378624351c12a8843fbaa5f3d43ac
