87f32f8a7d82190ecf7ad52cac391d75c0b46a5a
OLIKOGOGLA <olikogogla@gmail.com>
2026-06-17 06:37:20 -0700

    Add files via upload

---
+++ level 011/cklaswork/cw.py
@@ -0,0 +1,47 @@
+<<<<<<< HEAD
+for i in range(1, 100):
+    print(i)
+
+for i in range(5, 50, 5):
+    print(i)
+
+for i in range(50):
+    print(" goa best ")
+
+for i in range(25):
+    print(i  +  2 )
+
+for i in range(100):
+    print(i  *  2 )
+
+for i in range(1, 500, 10):
+    print(i  *  25 )
+
+
+name = input()
+for i in range(1, 15):
+    print(str(i), name)
+=======
+for i in range(1, 100):
+    print(i)
+
+for i in range(5, 50, 5):
+    print(i)
+
+for i in range(50):
+    print(" goa best ")
+
+for i in range(25):
+    print(i  +  2 )
+
+for i in range(100):
+    print(i  *  2 )
+
+for i in range(1, 500, 10):
+    print(i  *  25 )
+
+
+name = input()
+for i in range(1, 15):
+    print(str(i), name)
+>>>>>>> 8a15946c560378624351c12a8843fbaa5f3d43ac
---
+++ level 011/homework/hw.py
@@ -0,0 +1,52 @@
+<<<<<<< HEAD
+for i in range(1, 100):
+    print(i)
+
+for i in range(5, 50, 5):
+    print(i)
+
+for i in range(50):
+    print(" goa best ")
+
+for i in range(25):
+    print(i  +  7 )
+
+for i in range(100):
+    print(i  *  7 )
+
+for i in range(1, 300, 15):
+    print(i  *  50 )
+
+
+name = input()
+for i in range(1, 15):
+    print(str(i), name)
+
+for i in range(1 , 1000000 ):
+=======
+for i in range(1, 100):
+    print(i)
+
+for i in range(5, 50, 5):
+    print(i)
+
+for i in range(50):
+    print(" goa best ")
+
+for i in range(25):
+    print(i  +  7 )
+
+for i in range(100):
+    print(i  *  7 )
+
+for i in range(1, 300, 15):
+    print(i  *  50 )
+
+
+name = input()
+for i in range(1, 15):
+    print(str(i), name)
+
+for i in range(1 , 1000000 ):
+>>>>>>> 8a15946c560378624351c12a8843fbaa5f3d43ac
+    print(i)
